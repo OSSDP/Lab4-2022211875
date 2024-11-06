caa40ef2d4c30ec76ef9c81424a549518d446d4f
tianruchen <2095106195@qq.com>
2024-11-06 14:51:49 +0800

    lab4

--- src/main/java/com/it/Solution7Test.java
+++ src/main/java/com/it/Solution7Test.java
@@ -39,7 +39,7 @@ public class Solution7Test {
      */
     private static void testSmallestStringWithSwaps1(Solution7 solution) {
         String s = "dcab";
-        List<List<Integer>> pairs = Arrays.asList(Arrays.asList(0,.), Arrays.asList(3,0));
+        List<List<Integer>> pairs = Arrays.asList(Arrays.asList(0,3), Arrays.asList(1,2));
         String result = solution.smallestStringWithSwaps(s, pairs);
         System.out.println(result); //预期输出:"bacd"
     }
